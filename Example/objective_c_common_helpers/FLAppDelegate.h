388e4dd957e927ff0019dbf317a7f26183b11ed7
Hiren <ajay@emptyops.com>
2019-03-06 15:48:32 +0530

    Initial commit

---
+++ Example/objective_c_common_helpers/FLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FLAppDelegate.h
+//  objective_c_common_helpers
+//
+//  Created by ajayempty on 03/06/2019.
+//  Copyright (c) 2019 ajayempty. All rights reserved.
+//
+
+@import UIKit;
+
+@interface FLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
